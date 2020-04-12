8519c1df15279f9fe25e7df1adde1467ced0cb33
Alexandros Mironidis <alex.m@novoville.ocm>
2020-04-12 11:42:47 +0300

    Updates

---
+++ precache-manifest.5456ae051d52128424075ca3448cdd2f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "323ded129771994e3d292b9ca67a5e55",
+    "url": "/list_on_the_fridge/index.html"
+  },
+  {
+    "revision": "29fc80769da170a8e779",
+    "url": "/list_on_the_fridge/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c8a13b44c4cca0e3f3dd",
+    "url": "/list_on_the_fridge/static/js/2.10fb4db5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/list_on_the_fridge/static/js/2.10fb4db5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29fc80769da170a8e779",
+    "url": "/list_on_the_fridge/static/js/main.7d28e504.chunk.js"
+  },
+  {
+    "revision": "a4d486c9869c39155db2",
+    "url": "/list_on_the_fridge/static/js/runtime-main.cc303245.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/list_on_the_fridge/static/media/logo.5d5d9eef.svg"
+  }
+]);
