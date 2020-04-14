4ed63d8de4aee41dbdb307ede2a9612b8967c03d
Alexandros Mironidis <alex.m@novoville.ocm>
2020-04-14 14:58:35 +0300

    add routing functionallity with example components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import LogoAppBar from "./components/AppBar.js";
 import BottomNavigate from "./components/BottomNavigation.js";
 import "./App.css";
 import logoOnScreen from "./assets/logoOnScreen.png";
+import Categories from "./components/Categories.js";
+import SimpleList from "./components/SimpleList.js";
+import Products from "./components/Products.js";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
@@ -10,7 +15,23 @@ function App() {
 
       <header className="App-header">
         <img src={logoOnScreen} className="App-logo" alt="logo" />
-        <BottomNavigate />
+        <Router>
+          <div>
+            <Switch>
+              <Route path="/list_on_the_fridge/SimpleList">
+                <SimpleList />
+              </Route>
+              <Route path="/list_on_the_fridge/Categories">
+                <Categories />
+              </Route>
+              <Route path="/list_on_the_fridge/Products">
+                <Products />
+              </Route>
+            </Switch>
+          </div>
+
+          <BottomNavigate />
+        </Router>
       </header>
     </div>
   );
--- src/components/BottomNavigation.js
+++ src/components/BottomNavigation.js
@@ -4,6 +4,7 @@ import {
   MuiThemeProvider,
   createMuiTheme,
 } from "@material-ui/core/styles";
+import { Link } from "react-router-dom";
 import BottomNavigation from "@material-ui/core/BottomNavigation";
 import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
 import FormatListNumberedIcon from "@material-ui/icons/FormatListNumbered";
@@ -22,30 +23,44 @@ const useStyles = makeStyles({
     overflow: "hidden",
     position: "fixed",
     bottom: 0,
-    backgroundColor: "#5da2d5",
+    backgroundColor: "#435E77",
   },
 });
 
 export default function BottomNavigate() {
   const classes = useStyles();
-  const [value, setValue] = React.useState(0);
+  const initialState = () => Number(localStorage.getItem("TabValue") || 0);
+  const [value, setValue] = React.useState(initialState);
 
   return (
     <MuiThemeProvider theme={theme}>
       <BottomNavigation
         value={value}
         onChange={(event, newValue) => {
+          localStorage.setItem("TabValue", newValue);
           setValue(newValue);
         }}
         showLabels
         className={classes.root}
       >
         <BottomNavigationAction
+          component={Link}
+          to="/list_on_the_fridge/SimpleList"
           label="List"
           icon={<FormatListNumberedIcon />}
         />
-        <BottomNavigationAction label="Categories" icon={<ViewAgendaIcon />} />
-        <BottomNavigationAction label="Products" icon={<FastfoodIcon />} />
+        <BottomNavigationAction
+          component={Link}
+          to="/list_on_the_fridge/Categories"
+          label="Categories"
+          icon={<ViewAgendaIcon />}
+        />
+        <BottomNavigationAction
+          component={Link}
+          to="/list_on_the_fridge/Products"
+          label="Products"
+          icon={<FastfoodIcon />}
+        />
       </BottomNavigation>
     </MuiThemeProvider>
   );
